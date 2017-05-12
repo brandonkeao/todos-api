8c9130204dbbb57b19b975898fe89aa4a9e1aa38
brandonkeao <brandon.keao@gmail.com>
2017-05-12 16:10:03 -0600

    Coded Intem Controller to have the items spec pass.

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -76,7 +76,7 @@ RSpec.describe 'Items API' do
 			before { post "/todos/#{todo_id}/items", params: valid_attributes }
 
 			it 'returns status code 201' do
-				expect(response).to have_http_status(200)
+				expect(response).to have_http_status(201)
 			end
 		end
 
@@ -95,16 +95,19 @@ RSpec.describe 'Items API' do
 
 	# Test suite for PUT /todos/:todo_id/items/:id -> Put AN items into or attach it to a Todo
 	describe 'PUT /todos/:todo_id/items/:id' do
-		let(:valid_attributes) { { name: 'Mozart' } }
+		let(:valid_attributes) { { name: 'Mozart', done: false } }
 
 		before { put "/todos/#{todo_id}/items/#{id}", params: valid_attributes }
 
 		context 'when item exists' do
 
 			it 'returns status code 204' do
+				expect(response).to have_http_status(204)
 			end
 
 			it 'updates the item' do
+				updated_item = Item.find(id)
+        		expect(updated_item.name).to match(/Mozart/)
 			end
 
 		end
@@ -117,7 +120,7 @@ RSpec.describe 'Items API' do
 			end
 
 			it 'returns a not found message' do
-				expect(response.body).to match(/Couldn't find item/)
+				expect(response.body).to match(/Couldn't find Item/)
 			end
 		end	
 	end
